55b6bc386a9662880a592aa7f6e487f231c75472
Sebastian Redl <sebastian.redl@getdesigned.at>
2013-05-14 22:57:11 -0600

    Fix indentation.

--- include/accent/support/concepts.hpp
+++ include/accent/support/concepts.hpp
@@ -8,68 +8,68 @@ namespace accent { namespace support {
 
   namespace detail {
 
-      // Test for conversion to bool and operator !.
-      template <typename T>
-      std::true_type has_proper_boolean_conversion_helper(const T* t,
-          decltype(*t ? true : true) = true);
-      std::false_type has_proper_boolean_conversion_helper(...);
-      template <typename T>
-      struct has_proper_boolean_conversion
-        : decltype(has_proper_boolean_conversion_helper((T*)0))
-      {};
+    // Test for conversion to bool and operator !.
+    template <typename T>
+    std::true_type has_proper_boolean_conversion_helper(const T* t,
+        decltype(*t ? true : true) = true);
+    std::false_type has_proper_boolean_conversion_helper(...);
+    template <typename T>
+    struct has_proper_boolean_conversion
+      : decltype(has_proper_boolean_conversion_helper((T*)0))
+    {};
 
-      // Test for nested type traversal and its convertibility to T.
-      template <typename T, typename R>
-      typename std::is_base_of<T, typename R::traversal>::type
-          traversal_supports_helper(R*);
-      template <typename T>
-      std::false_type traversal_supports_helper(...);
-      template <typename T, typename R>
-      struct traversal_supports
-        : decltype(traversal_supports_helper<T>((R*)0))
-      {};
+    // Test for nested type traversal and its convertibility to T.
+    template <typename T, typename R>
+    typename std::is_base_of<T, typename R::traversal>::type
+        traversal_supports_helper(R*);
+    template <typename T>
+    std::false_type traversal_supports_helper(...);
+    template <typename T, typename R>
+    struct traversal_supports
+      : decltype(traversal_supports_helper<T>((R*)0))
+    {};
 
-      // Test for empty() that returns convertible to bool.
-      template <typename R>
-      auto has_empty_helper(const R* r)
-          -> typename std::is_convertible<decltype(r->empty()), bool>::type;
-      std::false_type has_empty_helper(...);
-      template <typename R>
-      struct has_empty
-        : decltype(has_empty_helper((R*)0))
-      {};
+    // Test for empty() that returns convertible to bool.
+    template <typename R>
+    auto has_empty_helper(const R* r)
+        -> typename std::is_convertible<decltype(r->empty()), bool>::type;
+    std::false_type has_empty_helper(...);
+    template <typename R>
+    struct has_empty
+      : decltype(has_empty_helper((R*)0))
+    {};
 
-      // Test for nested type value_type.
-      template <typename R>
-      std::true_type has_value_type_helper(const R* r,
-                                           const typename R::value_type* = 0);
-      std::false_type has_value_type_helper(...);
-      template <typename R>
-      struct has_value_type
-        : decltype(has_value_type_helper((R*)0))
-      {};
+    // Test for nested type value_type.
+    template <typename R>
+    std::true_type has_value_type_helper(const R* r,
+                                         const typename R::value_type* = 0);
+    std::false_type has_value_type_helper(...);
+    template <typename R>
+    struct has_value_type
+      : decltype(has_value_type_helper((R*)0))
+    {};
 
-      // Test for front() that returns non-void.
-      template <typename R>
-      auto has_front_helper(const R* r)
-          -> typename std::conditional<
-              std::is_same<decltype(r->front()), void>::value,
-              std::false_type, std::true_type>::type;
-      std::false_type has_front_helper(...);
-      template <typename R>
-      struct has_front
-        : decltype(has_front_helper((R*)0))
-      {};
+    // Test for front() that returns non-void.
+    template <typename R>
+    auto has_front_helper(const R* r)
+        -> typename std::conditional<
+            std::is_same<decltype(r->front()), void>::value,
+            std::false_type, std::true_type>::type;
+    std::false_type has_front_helper(...);
+    template <typename R>
+    struct has_front
+      : decltype(has_front_helper((R*)0))
+    {};
 
-      // Test for drop_front().
-      template <typename R>
-      std::true_type has_drop_front_helper(R* r,
-          decltype(r->drop_front())* = 0);
-      std::false_type has_drop_front_helper(...);
-      template <typename R>
-      struct has_drop_front
-        : decltype(has_drop_front_helper((R*)0))
-      {};
+    // Test for drop_front().
+    template <typename R>
+    std::true_type has_drop_front_helper(R* r,
+        decltype(r->drop_front())* = 0);
+    std::false_type has_drop_front_helper(...);
+    template <typename R>
+    struct has_drop_front
+      : decltype(has_drop_front_helper((R*)0))
+    {};
 
   }
 
