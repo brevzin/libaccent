fd9ff7930ff6e0cad81d0390ab410a3a7c7d9fbd
Sebastian Redl <sebastian.redl@getdesigned.at>
2013-06-17 23:51:07 +0200

    Extend the vector_range tests to cover forward range functionality.

--- tests/ranges/vector_range.cpp
+++ tests/ranges/vector_range.cpp
@@ -61,3 +61,54 @@ TEST(VectorRange, TwoElementVector_DropFront_DropFront_Empty) {
   r.drop_front();
   ASSERT_TRUE(r.empty());
 }
+
+TEST(VectorRange, EmptyVector_InvalidAtFront) {
+  ivec v = {};
+  auto r = adapt(v).all();
+  auto p = r.at_front();
+  ASSERT_FALSE(p.valid());
+}
+
+TEST(VectorRange, OneElementVector_AtFrontIsElement) {
+  ivec v = { 1 };
+  auto r = adapt(v).all();
+  auto p = r.at_front();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(1, *p);
+}
+
+TEST(VectorRange, TwoElementVector_DropFront_AtFrontIsSecondElement) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.drop_front();
+  auto p = r.at_front();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(2, *p);
+}
+
+TEST(VectorRange, ThreeElementVector_FromSecondElement_ContainsLastTwo) {
+  ivec v = { 1, 2, 3 };
+  auto r = adapt(v).all();
+  auto s = r;
+  s.drop_front();
+  r = r.from(s.at_front());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(2, r.front());
+  r.drop_front();
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(3, r.front());
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(VectorRange, ThreeElementVector_UntilSecondElement_ContainsFirst) {
+  ivec v = { 1, 2, 3 };
+  auto r = adapt(v).all();
+  auto s = r;
+  s.drop_front();
+  r = r.until(s.at_front());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(1, r.front());
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
