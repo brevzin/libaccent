3290db960f16f2300d20a5ddb7b04420775e0aa7
Sebastian Redl <sebastian.redl@getdesigned.at>
2013-06-22 11:40:38 +0200

    Extend projected_range to forward ranges and expand tests.

--- include/accent/ranges/project.hpp
+++ include/accent/ranges/project.hpp
@@ -1,25 +1,80 @@
 #ifndef LIBACCENT_RANGES_PROJECT_HPP
 #define LIBACCENT_RANGES_PROJECT_HPP
 
+#include "accent/tags.hpp"
 #include "accent/support/concepts.hpp"
 #include "accent/support/range_base.hpp"
+#include "accent/support/tag_functions.hpp"
 #include <boost/compressed_pair.hpp>
+#include <boost/optional.hpp>
 #include <type_traits>
 #include <utility>
 
 namespace accent { namespace ranges {
 
+  template <typename Inner, typename Projection>
+  class projected_range;
+
+  template <typename Inner, typename Projection>
+  class projected_position {
+    Inner inner_position;
+    // Needed to ensure default-constructability. Should optimize for when
+    // the projection is default-constructible anyway.
+    boost::optional<Projection> projection;
+
+  public:
+    using value_type =
+        typename std::decay<decltype(std::declval<Projection>()(
+                                         *std::declval<Inner>()))
+                           >::type;
+
+    projected_position() = default;
+
+    projected_position(Inner inner, Projection projection)
+      : inner_position(std::move(inner)), projection(std::move(projection)) {}
+
+    Inner inner() const { return inner_position; }
+
+    bool valid() const { return inner().valid(); }
+    explicit operator bool() const { return valid(); }
+
+    auto operator *() const -> decltype((*projection)(*inner())) {
+      return (*projection)(*inner());
+    }
+  };
+  template <typename Inner, typename Projection>
+  bool operator ==(const projected_position<Inner, Projection>& l,
+                   const projected_position<Inner, Projection>& r) {
+    // Must assume that functions are equal; there is no way to compare them.
+    return l.inner() == r.inner();
+  }
+  template <typename Inner, typename Projection>
+  bool operator !=(const projected_position<Inner, Projection>& l,
+                   const projected_position<Inner, Projection>& r) {
+    return !(l == r);
+  }
+
   namespace detail {
 
-    template <typename Inner, typename Projection>
-    class projected_range :
-        public support::range_base<projected_range<Inner, Projection>> {
+    template <typename Derived, typename Inner,
+              typename Projection, typename Tag>
+    class projected_range_impl;
+
+    template <typename Derived, typename Inner, typename Projection>
+    class projected_range_impl<Derived, Inner, Projection,
+                               single_pass_traversal_tag>
+        : public support::range_base<Derived> {
       boost::compressed_pair<Inner, Projection> data;
 
+    protected:
       const Inner& inner() const { return data.first(); }
       Inner& inner() { return data.first(); }
 
       const Projection& projection() const { return data.second(); }
+
+      projected_range_impl(Inner inner, Projection projection)
+        : data(std::move(inner), std::move(projection)) {}
+      ~projected_range_impl() = default;
 
     public:
       using value_type =
@@ -27,9 +82,6 @@ namespace accent { namespace ranges {
                                            std::declval<Inner>().front()))
                              >::type;
       using traversal = support::traversal_of<Inner>;
-
-      projected_range(Inner inner, Projection projection)
-        : data(std::move(inner), std::move(projection)) {}
 
       bool empty() const { return inner().empty(); }
 
@@ -40,11 +92,85 @@ namespace accent { namespace ranges {
       }
     };
 
+    template <typename Derived, typename Inner, typename Projection>
+    class projected_range_impl<Derived, Inner, Projection,
+                               forward_traversal_tag>
+        : public projected_range_impl<Derived, Inner, Projection,
+                                      single_pass_traversal_tag> {
+      using base = projected_range_impl<Derived, Inner, Projection,
+                                        single_pass_traversal_tag>;
+    protected:
+      using base::base;
+      ~projected_range_impl() = default;
+
+    public:
+      using position = projected_position<support::position_of<Inner>,
+                                          Projection>;
+
+      position at_front() const {
+        return { this->inner().at_front(), this->projection() };
+      }
+      Derived from(position p) const {
+        return Derived(this->inner().from(p.inner()), this->projection());
+      }
+      auto until(position p) const
+          -> projected_range<decltype(this->inner().until(p.inner())),
+                             Projection> {
+        return { this->inner().until(p.inner()), this->projection() };
+      }
+    };
+
+    template <typename Derived, typename Inner, typename Projection>
+    class projected_range_impl<Derived, Inner, Projection,
+                               bidirectional_traversal_tag>
+        : public projected_range_impl<Derived, Inner, Projection,
+                                      forward_traversal_tag> {
+      using base = projected_range_impl<Derived, Inner, Projection,
+                                        forward_traversal_tag>;
+    protected:
+      using base::base;
+      ~projected_range_impl() = default;
+
+    public:
+    };
+
+    template <typename Derived, typename Inner, typename Projection>
+    class projected_range_impl<Derived, Inner, Projection,
+                               random_access_traversal_tag>
+        : public projected_range_impl<Derived, Inner, Projection,
+                                      bidirectional_traversal_tag> {
+      using base = projected_range_impl<Derived, Inner, Projection,
+                                        bidirectional_traversal_tag>;
+    protected:
+      using base::base;
+      ~projected_range_impl() = default;
+
+    public:
+    };
+
   }
 
   template <typename Inner, typename Projection>
-  detail::projected_range<Inner, Projection> project(Inner i, Projection p) {
-    return {std::move(i), std::move(p)};
+  class projected_range
+    : public detail::projected_range_impl<projected_range<Inner, Projection>,
+                                          Inner, Projection,
+                                          support::traversal_of<Inner>>
+  {
+    using base = detail::projected_range_impl<projected_range<Inner,Projection>,
+                                              Inner, Projection,
+                                              support::traversal_of<Inner>>;
+
+  public:
+      projected_range(Inner inner, Projection projection)
+        : base(std::move(inner), std::move(projection)) {}
+  };
+
+  template <typename Inner, typename RegularUnaryFunction>
+  projected_range<Inner, RegularUnaryFunction>
+  project(Inner i, RegularUnaryFunction p) {
+    static_assert(support::SinglePassRange<Inner>(),
+                  "wrapped range does not model SinglePassRange");
+    return { std::move(i), std::move(p) };
   }
 
 }}
--- tests/ranges/project.cpp
+++ tests/ranges/project.cpp
@@ -13,38 +13,130 @@ struct grab_i {
   const int& operator ()(const S& s) const { return s.i; }
 };
 
-static_assert(SinglePassRange<ranges::detail::projected_range<
-                                  iterator_range<svec::iterator>, grab_i>>(),
-              "projected single pass range does not model SinglePassRange");
+using s_range = iterator_range<svec::iterator>;
+using i_of_s_range = projected_range<s_range, grab_i>;
+using sp_s_range = ranges::detail::as_single_pass_range<s_range>;
+using i_of_sp_s_range = projected_range<sp_s_range, grab_i>;
 
-TEST(Project, emptySource_emptyProjected) {
+static_assert(ForwardRange<i_of_s_range>(),
+              "projected forward range does not model ForwardRange");
+static_assert(SinglePassRange<i_of_sp_s_range>(),
+              "projected single pass range does not model SinglePassRange");
+static_assert(!ForwardRange<i_of_sp_s_range>(),
+              "projected single pass range claims to model ForwardRange");
+
+TEST(SpProject, emptySource_emptyProjected) {
+  svec v = {};
+  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(FwdProject, emptySource_emptyProjected) {
   svec v = {};
   auto r = project(adapt(v).all(), grab_i());
   ASSERT_TRUE(r.empty());
 }
 
-TEST(Project, oneElementSource_notEmptyProjected) {
+TEST(SpProject, oneElementSource_notEmptyProjected) {
+  svec v = { {1} };
+  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  ASSERT_FALSE(r.empty());
+}
+
+TEST(FwdProject, oneElementSource_notEmptyProjected) {
   svec v = { {1} };
   auto r = project(adapt(v).all(), grab_i());
   ASSERT_FALSE(r.empty());
 }
 
-TEST(Project, elementRead_projectedRead) {
+TEST(SpProject, elementRead_projectedRead) {
+  svec v = { {1} };
+  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  ASSERT_EQ(1, r.front());
+}
+
+TEST(FwdProject, elementRead_projectedRead) {
   svec v = { {1} };
   auto r = project(adapt(v).all(), grab_i());
   ASSERT_EQ(1, r.front());
 }
 
-TEST(Project, projectedWrite_elementWrite) {
+TEST(SpProject, projectedWrite_elementWrite) {
+  svec v = { {1} };
+  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  r.front() = 2;
+  ASSERT_EQ(2, v.front().i);
+}
+
+TEST(FwdProject, projectedWrite_elementWrite) {
   svec v = { {1} };
   auto r = project(adapt(v).all(), grab_i());
   r.front() = 2;
   ASSERT_EQ(2, v.front().i);
 }
 
-TEST(Project, oneElementSource_dropFront_emptyProjected) {
+TEST(SpProject, oneElementSource_dropFront_emptyProjected) {
+  svec v = { {1} };
+  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(FwdProject, oneElementSource_dropFront_emptyProjected) {
   svec v = { {1} };
   auto r = project(adapt(v).all(), grab_i());
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(FwdProject, emptySource_atFrontIsInvalid) {
+  svec v = {};
+  auto r = project(adapt(v).all(), grab_i());
+  auto p = r.at_front();
+  ASSERT_FALSE(p.valid());
+}
+
+TEST(FwdProject, oneElementSource_atFrontIsElement) {
+  svec v = { {1} };
+  auto r = project(adapt(v).all(), grab_i());
+  auto p = r.at_front();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(1, *p);
+}
+
+
+TEST(FwdProject, twoElementSource_dropFront_atFrontIsSecondElement) {
+  svec v = { {1}, {2} };
+  auto r = project(adapt(v).all(), grab_i());
+  r.drop_front();
+  auto p = r.at_front();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(2, *p);
+}
+
+TEST(FwdProject, threeElementSource_fromSecondElement_containsLastTwo) {
+  svec v = { {1}, {2}, {3} };
+  auto r = project(adapt(v).all(), grab_i());
+  auto s = r;
+  s.drop_front();
+  r = r.from(s.at_front());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(2, r.front());
+  r.drop_front();
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(3, r.front());
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(FwdProject, threeElementSource_untilSecondElement_containsFirst) {
+  svec v = { {1}, {2}, {3} };
+  auto r = project(adapt(v).all(), grab_i());
+  auto s = r;
+  s.drop_front();
+  r = r.until(s.at_front());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(1, r.front());
   r.drop_front();
   ASSERT_TRUE(r.empty());
 }
