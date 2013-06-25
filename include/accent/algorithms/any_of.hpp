a5490c2805ddd9888fa364a4351d4eb02ac1a2f9
Sebastian Redl <sebastian.redl@getdesigned.at>
2013-06-25 21:23:32 +0200

    Implement any_of in terms of drop_until.

--- include/accent/algorithms/any_of.hpp
+++ include/accent/algorithms/any_of.hpp
@@ -1,36 +1,13 @@
 #ifndef LIBACCENT_ALGORITHMS_ANY_OF_HPP
 #define LIBACCENT_ALGORITHMS_ANY_OF_HPP
 
-#include "accent/support/concepts.hpp"
-#include "accent/support/utility.hpp"
+#include "accent/algorithms/drop_conditional.hpp"
 
 namespace accent { namespace algorithms {
 
-  namespace detail {
-
-    template <typename ReadableSinglePassRange, typename Predicate>
-    bool any_of(ReadableSinglePassRange, Predicate, std::false_type);
-
-    template <typename ReadableSinglePassRange, typename Predicate>
-    bool any_of(ReadableSinglePassRange r, Predicate p, std::true_type) {
-      for (; r; r.drop_front()) {
-        if (p(r.front()))
-          return true;
-      }
-      return false;
-    }
-
-  }
-
   template <typename ReadableSinglePassRange, typename Predicate>
   bool any_of(ReadableSinglePassRange r, Predicate p) {
-    static_assert(support::SinglePassRange<ReadableSinglePassRange>(),
-                  "any_of requires a SinglePassRange");
-    static_assert(support::ReadableRange<ReadableSinglePassRange>(),
-                  "any_of requires a ReadableRange");
-    return detail::any_of(r, p,
-        support::bool_<support::SinglePassRange<ReadableSinglePassRange>() &&
-                       support::ReadableRange<ReadableSinglePassRange>()>());
+    return !drop_until(r, p).empty();
   }
 
 }}
