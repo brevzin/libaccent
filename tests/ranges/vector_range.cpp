a89caf79dd659f31e68996993b15fbbf6953fbd9
Sebastian Redl <sebastian.redl@getdesigned.at>
2013-07-27 22:35:06 +0200

    Tests for vector_range as bidirectional.

--- tests/ranges/vector_range.cpp
+++ tests/ranges/vector_range.cpp
@@ -109,6 +109,114 @@ TEST(VectorRange, ThreeElementVector_SetFrontSecondElement_ContainsLastTwo) {
   ASSERT_TRUE(r.empty());
 }
 
+TEST(VectorRange, OneElementVector_BackIsFirstElement) {
+  ivec v = { 1 };
+  auto r = adapt(v).all();
+  ASSERT_EQ(1, r.back());
+}
+
+TEST(VectorRange, OneElementVector_DropBack_Empty) {
+  ivec v = { 1 };
+  auto r = adapt(v).all();
+  r.drop_back();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(VectorRange, TwoElementVector_DropBack_NotEmpty) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.drop_back();
+  ASSERT_FALSE(r.empty());
+}
+
+TEST(VectorRange, TwoElementVector_BackIsSecondElement) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  ASSERT_EQ(2, r.back());
+}
+
+TEST(VectorRange, TwoElementVector_DropBack_BackIsFirstElement) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.drop_back();
+  ASSERT_EQ(1, r.back());
+}
+
+TEST(VectorRange, TwoElementVector_DropBack_DropBack_Empty) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.drop_back();
+  r.drop_back();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(VectorRange, TwoElementVector_DropFront_DropBack_Empty) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.drop_front();
+  r.drop_back();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(VectorRange, TwoElementVector_DropBack_DropFront_Empty) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.drop_back();
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(VectorRange, EmptyVector_InvalidAtBack) {
+  ivec v = {};
+  auto r = adapt(v).all();
+  auto p = r.at_back();
+  ASSERT_FALSE(p.valid());
+}
+
+TEST(VectorRange, OneElementVector_AtBackIsElement) {
+  ivec v = { 1 };
+  auto r = adapt(v).all();
+  auto p = r.at_back();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(1, *p);
+}
+
+TEST(VectorRange, OneElementVector_DropBack_InvalidAtBack) {
+  ivec v = { 1 };
+  auto r = adapt(v).all();
+  r.drop_front();
+  auto p = r.at_back();
+  ASSERT_FALSE(p.valid());
+}
+
+TEST(VectorRange, TwoElementVector_AtBackIsSecondElement) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  auto p = r.at_back();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(2, *p);
+}
+
+TEST(VectorRange, TwoElementVector_DropBack_AtBackIsFirstElement) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.drop_back();
+  auto p = r.at_back();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(1, *p);
+}
+
+TEST(VectorRange, SetBackAtBack_SameElements) {
+  ivec v = { 1, 2 };
+  auto r = adapt(v).all();
+  r.set_back(r.at_back());
+  ASSERT_FALSE(r.empty());
+  r.drop_front();
+  ASSERT_FALSE(r.empty());
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
+
 TEST(VectorRange, ThreeElementVector_SetBackSecondElement_ContainsFirstTwo) {
   ivec v = { 1, 2, 3 };
   auto r = adapt(v).all();
