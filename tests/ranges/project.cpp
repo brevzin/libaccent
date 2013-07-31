7ca805a1618369ba32f6be28e973649932c37c42
Sebastian Redl <sebastian.redl@getdesigned.at>
2013-07-31 18:14:53 +0200

    Simplifications and additional tests for project.

--- tests/ranges/project.cpp
+++ tests/ranges/project.cpp
@@ -31,153 +31,85 @@ static_assert(SinglePassRange<i_of_sp_s_range>(),
 static_assert(!ForwardRange<i_of_sp_s_range>(),
               "projected single pass range claims to model ForwardRange");
 
-TEST(SpProject, emptySource_emptyProjected) {
+i_of_sp_s_range sp_i(svec& v) {
+  return project(as_single_pass(adapt(v).all()), grab_i());
+}
+
+i_of_fwd_s_range fwd_i(svec& v) {
+  return project(as_forward(adapt(v).all()), grab_i());
+}
+
+i_of_s_range bidi_i(svec& v) {
+  return project(adapt(v).all(), grab_i());
+}
+
+TEST(Project, emptySource_emptyProjected) {
   svec v = {};
   auto r = project(as_single_pass(adapt(v).all()), grab_i());
   ASSERT_TRUE(r.empty());
 }
 
-TEST(FwdProject, emptySource_emptyProjected) {
-  svec v = {};
-  auto r = project(as_forward(adapt(v).all()), grab_i());
-  ASSERT_TRUE(r.empty());
-}
-
-TEST(BidiProject, emptySource_emptyProjected) {
-  svec v = {};
-  auto r = project(adapt(v).all(), grab_i());
-  ASSERT_TRUE(r.empty());
-}
-
-TEST(SpProject, oneElementSource_notEmptyProjected) {
+TEST(Project, oneElementSource_notEmptyProjected) {
   svec v = { {1} };
-  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  auto r = sp_i(v);
   ASSERT_FALSE(r.empty());
 }
 
-TEST(FwdProject, oneElementSource_notEmptyProjected) {
+TEST(Project, elementRead_projectedRead) {
   svec v = { {1} };
-  auto r = project(as_forward(adapt(v).all()), grab_i());
-  ASSERT_FALSE(r.empty());
-}
-
-TEST(BidiProject, oneElementSource_notEmptyProjected) {
-  svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
-  ASSERT_FALSE(r.empty());
-}
-
-TEST(SpProject, elementRead_projectedRead) {
-  svec v = { {1} };
-  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  auto r = sp_i(v);
   ASSERT_EQ(1, r.front());
 }
 
-TEST(FwdProject, elementRead_projectedRead) {
+TEST(Project, projectedWrite_elementWrite) {
   svec v = { {1} };
-  auto r = project(as_forward(adapt(v).all()), grab_i());
-  ASSERT_EQ(1, r.front());
-}
-
-TEST(BidiProject, elementRead_projectedRead) {
-  svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
-  ASSERT_EQ(1, r.front());
-}
-
-TEST(SpProject, projectedWrite_elementWrite) {
-  svec v = { {1} };
-  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  auto r = sp_i(v);
   r.front() = 2;
   ASSERT_EQ(2, v.front().i);
 }
 
-TEST(FwdProject, projectedWrite_elementWrite) {
+TEST(Project, oneElementSource_dropFront_emptyProjected) {
   svec v = { {1} };
-  auto r = project(as_forward(adapt(v).all()), grab_i());
-  r.front() = 2;
-  ASSERT_EQ(2, v.front().i);
-}
-
-TEST(BidiProject, projectedWrite_elementWrite) {
-  svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
-  r.front() = 2;
-  ASSERT_EQ(2, v.front().i);
-}
-
-TEST(SpProject, oneElementSource_dropFront_emptyProjected) {
-  svec v = { {1} };
-  auto r = project(as_single_pass(adapt(v).all()), grab_i());
+  auto r = sp_i(v);
   r.drop_front();
   ASSERT_TRUE(r.empty());
 }
 
-TEST(FwdProject, oneElementSource_dropFront_emptyProjected) {
-  svec v = { {1} };
-  auto r = project(as_forward(adapt(v).all()), grab_i());
-  r.drop_front();
-  ASSERT_TRUE(r.empty());
-}
-
-TEST(BidiProject, oneElementSource_dropFront_emptyProjected) {
-  svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
-  r.drop_front();
-  ASSERT_TRUE(r.empty());
-}
-
-TEST(FwdProject, emptySource_atFrontIsInvalid) {
-  svec v = {};
-  auto r = project(as_forward(adapt(v).all()), grab_i());
-  auto p = r.at_front();
-  ASSERT_FALSE(p.valid());
-}
-
-TEST(BidiProject, emptySource_atFrontIsInvalid) {
-  svec v = {};
-  auto r = project(adapt(v).all(), grab_i());
-  auto p = r.at_front();
-  ASSERT_FALSE(p.valid());
-}
-
-TEST(FwdProject, oneElementSource_atFrontIsElement) {
-  svec v = { {1} };
-  auto r = project(as_forward(adapt(v).all()), grab_i());
-  auto p = r.at_front();
-  ASSERT_TRUE(p.valid());
-  ASSERT_EQ(1, *p);
-}
-
-TEST(BidiProject, oneElementSource_atFrontIsElement) {
-  svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
-  auto p = r.at_front();
-  ASSERT_TRUE(p.valid());
-  ASSERT_EQ(1, *p);
-}
-
-TEST(FwdProject, twoElementSource_dropFront_atFrontIsSecondElement) {
+TEST(Project, twoElementSource_dropFront_frontIsSecond) {
   svec v = { {1}, {2} };
-  auto r = project(as_forward(adapt(v).all()), grab_i());
+  auto r = sp_i(v);
+  r.drop_front();
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(2, r.front());
+}
+
+TEST(Project, emptySource_atFrontIsInvalid) {
+  svec v = {};
+  auto r = fwd_i(v);
+  auto p = r.at_front();
+  ASSERT_FALSE(p.valid());
+}
+
+TEST(Project, oneElementSource_atFrontIsElement) {
+  svec v = { {1} };
+  auto r = fwd_i(v);
+  auto p = r.at_front();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(1, *p);
+}
+
+TEST(Project, twoElementSource_dropFront_atFrontIsSecondElement) {
+  svec v = { {1}, {2} };
+  auto r = fwd_i(v);
   r.drop_front();
   auto p = r.at_front();
   ASSERT_TRUE(p.valid());
   ASSERT_EQ(2, *p);
 }
 
-TEST(BidiProject, twoElementSource_dropFront_atFrontIsSecondElement) {
-  svec v = { {1}, {2} };
-  auto r = project(adapt(v).all(), grab_i());
-  r.drop_front();
-  auto p = r.at_front();
-  ASSERT_TRUE(p.valid());
-  ASSERT_EQ(2, *p);
-}
-
-TEST(FwdProject, threeElementSource_setFrontSecondElement_containsLastTwo) {
+TEST(Project, threeElementSource_setFrontSecondElement_containsLastTwo) {
   svec v = { {1}, {2}, {3} };
-  auto r = project(as_forward(adapt(v).all()), grab_i());
+  auto r = fwd_i(v);
   auto s = r;
   s.drop_front();
   r.set_front(s.at_front());
@@ -190,76 +122,76 @@ TEST(FwdProject, threeElementSource_setFrontSecondElement_containsLastTwo) {
   ASSERT_TRUE(r.empty());
 }
 
-TEST(BidiProject, threeElementSource_setFrontSecondElement_containsLastTwo) {
-  svec v = { {1}, {2}, {3} };
-  auto r = project(adapt(v).all(), grab_i());
-  auto s = r;
-  s.drop_front();
-  r.set_front(s.at_front());
-  ASSERT_FALSE(r.empty());
-  ASSERT_EQ(2, r.front());
-  r.drop_front();
-  ASSERT_FALSE(r.empty());
-  ASSERT_EQ(3, r.front());
-  r.drop_front();
-  ASSERT_TRUE(r.empty());
-}
-
-TEST(BidiProject, elementReadBack_projectedRead) {
+TEST(Project, elementReadBack_projectedRead) {
   svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   ASSERT_EQ(1, r.back());
 }
 
-TEST(BidiProject, projectedWriteBack_elementWrite) {
+TEST(Project, projectedWriteBack_elementWrite) {
   svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   r.back() = 2;
   ASSERT_EQ(2, v.back().i);
 }
 
-TEST(BidiProject, oneElementSource_dropBack_emptyProjected) {
+TEST(Project, oneElementSource_dropBack_emptyProjected) {
   svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   r.drop_back();
   ASSERT_TRUE(r.empty());
 }
 
-TEST(BidiProject, emptySource_atBackIsInvalid) {
+TEST(Project, twoElementSource_backIsSecond) {
+  svec v = { {1}, {2} };
+  auto r = bidi_i(v);
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(2, r.back());
+}
+
+TEST(Project, twoElementSource_dropBack_backIsFirst) {
+  svec v = { {1}, {2} };
+  auto r = bidi_i(v);
+  r.drop_back();
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(1, r.back());
+}
+
+TEST(Project, emptySource_atBackIsInvalid) {
   svec v = {};
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   auto p = r.at_back();
   ASSERT_FALSE(p.valid());
 }
 
-TEST(BidiProject, oneElementSource_atBackIsElement) {
+TEST(Project, oneElementSource_atBackIsElement) {
   svec v = { {1} };
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   auto p = r.at_back();
   ASSERT_TRUE(p.valid());
   ASSERT_EQ(1, *p);
 }
 
-TEST(BidiProject, twoElementSource_atBackIsSecondElement) {
+TEST(Project, twoElementSource_atBackIsSecondElement) {
   svec v = { {1}, {2} };
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   auto p = r.at_back();
   ASSERT_TRUE(p.valid());
   ASSERT_EQ(2, *p);
 }
 
-TEST(BidiProject, twoElementSource_dropBack_atBackIsFirstElement) {
+TEST(Project, twoElementSource_dropBack_atBackIsFirstElement) {
   svec v = { {1}, {2} };
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   r.drop_back();
   auto p = r.at_back();
   ASSERT_TRUE(p.valid());
   ASSERT_EQ(1, *p);
 }
 
-TEST(BidiProject, threeElementSource_setBackSecondElement_containsFirstTwo) {
+TEST(Project, threeElementSource_setBackSecondElement_containsFirstTwo) {
   svec v = { {1}, {2}, {3} };
-  auto r = project(adapt(v).all(), grab_i());
+  auto r = bidi_i(v);
   auto s = r;
   s.drop_front();
   r.set_back(s.at_front());
