3907dd20b8bfed137186d8f849f857b749df8b8c
Sebastian Redl <sebastian.redl@getdesigned.at>
2013-07-31 17:23:03 +0200

    Fully implement the reversed range.

--- include/accent/ranges/reversed.hpp
+++ include/accent/ranges/reversed.hpp
@@ -1,18 +1,58 @@
 #ifndef LIBACCENT_RANGES_REVERSED_HPP
 #define LIBACCENT_RANGES_REVERSED_HPP
 
+#include "accent/support/range.hpp"
 #include <utility>
 
 namespace accent { namespace ranges {
 
+  namespace detail {
+
+    template <typename Derived, typename Inner, typename Tag>
+    class reversed_range_impl;
+
+    template <typename Derived, typename Inner>
+    class reversed_range_impl<Derived, Inner, bidirectional_traversal_tag>
+        : public support::range_base<Derived> {
+    protected:
+      Inner inner;
+
+      explicit reversed_range_impl(Inner inner) : inner(std::move(inner)) {}
+
+    public:
+      bool empty() const { return inner.empty(); }
+
+      auto front() const -> decltype(inner.back()) { return inner.back(); }
+      auto back() const -> decltype(inner.front()) { return inner.front(); }
+
+      void drop_front() { inner.drop_back(); }
+      void drop_back() { inner.drop_front(); }
+
+      using position = support::position_of<Inner>;
+
+      position at_front() const { return inner.at_back(); }
+      position at_back() const { return inner.at_front(); }
+
+      void set_front(position p) { inner.set_back(p); }
+      void set_back(position p) { inner.set_front(p); }
+    };
+
+  }
+
   template <typename Inner>
-  class reversed_range {
-    Inner inner;
+  class reversed_range
+      : public detail::reversed_range_impl<reversed_range<Inner>, Inner,
+                                           support::traversal_of<Inner>> {
+    using base = detail::reversed_range_impl<reversed_range, Inner,
+                                             support::traversal_of<Inner>>;
 
   public:
-    explicit reversed_range(Inner inner) : inner(std::move(inner)) {}
+    static_assert(support::BidirectionalRange<Inner>(),
+                  "reversed requires a bidirectional range");
 
-    auto front() const -> decltype(inner.back()) { return inner.back(); }
+    using traversal = support::traversal_of<Inner>;
+
+    explicit reversed_range(Inner inner) : base(std::move(inner)) {}
   };
 
   template <typename BidirectionalRange>
---
+++ tests/ranges/reversed.cpp
@@ -0,0 +1,116 @@
+#include <accent/ranges/reversed.hpp>
+#include "test.hpp"
+
+TEST(Reversed, SourceEmpty_TargetEmpty) {
+  vec v = {};
+  auto r = reversed(v.bidi());
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(Reversed, OneElement_FrontIsElement) {
+  vec v = { 1 };
+  auto r = reversed(v.bidi());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(1, r.front());
+}
+
+TEST(Reversed, OneElement_BackIsElement) {
+  vec v = { 1 };
+  auto r = reversed(v.bidi());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(1, r.back());
+}
+
+TEST(Reversed, TwoElements_FrontIsSecond) {
+  vec v = { 1, 2 };
+  auto r = reversed(v.bidi());
+  ASSERT_EQ(2, r.front());
+}
+
+TEST(Reversed, TwoElements_BackIsFirst) {
+  vec v = { 1, 2 };
+  auto r = reversed(v.bidi());
+  ASSERT_EQ(1, r.back());
+}
+
+TEST(Reversed, OneElement_DropFront_Empty) {
+  vec v = { 1 };
+  auto r = reversed(v.bidi());
+  r.drop_front();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(Reversed, OneElement_DropBack_Empty) {
+  vec v = { 1 };
+  auto r = reversed(v.bidi());
+  r.drop_back();
+  ASSERT_TRUE(r.empty());
+}
+
+TEST(Reversed, TwoElements_DropFront_FrontIsFirst) {
+  vec v = { 1, 2 };
+  auto r = reversed(v.bidi());
+  r.drop_front();
+  ASSERT_EQ(1, r.front());
+}
+
+TEST(Reversed, TwoElements_DropBack_BackIsSecond) {
+  vec v = { 1, 2 };
+  auto r = reversed(v.bidi());
+  r.drop_back();
+  ASSERT_EQ(2, r.back());
+}
+
+TEST(Reversed, Empty_AtFrontIsInvalid) {
+  vec v = {};
+  auto r = reversed(v.bidi());
+  ASSERT_FALSE(r.at_front().valid());
+}
+
+TEST(Reversed, Empty_AtBackIsInvalid) {
+  vec v = {};
+  auto r = reversed(v.bidi());
+  ASSERT_FALSE(r.at_back().valid());
+}
+
+TEST(Reversed, TwoElements_AtFrontIsSecond) {
+  vec v = { 1, 2 };
+  auto r = reversed(v.bidi());
+  auto p = r.at_front();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(2, *p);
+}
+
+TEST(Reversed, TwoElements_AtBackIsFirst) {
+  vec v = { 1, 2 };
+  auto r = reversed(v.bidi());
+  auto p = r.at_back();
+  ASSERT_TRUE(p.valid());
+  ASSERT_EQ(1, *p);
+}
+
+TEST(Reversed, ThreeElements_SetFrontToCenter_HasFirstTwo) {
+  vec v = { 1, 2, 3 };
+  auto r = reversed(v.bidi());
+  auto s = r;
+  s.drop_back();
+  r.set_front(s.at_back());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(2, r.front());
+  r.drop_front();
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(1, r.front());
+}
+
+TEST(Reversed, ThreeElements_SetBackToCenter_HasLastTwo) {
+  vec v = { 1, 2, 3 };
+  auto r = reversed(v.bidi());
+  auto s = r;
+  s.drop_front();
+  r.set_back(s.at_front());
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(2, r.back());
+  r.drop_back();
+  ASSERT_FALSE(r.empty());
+  ASSERT_EQ(3, r.back());
+}
