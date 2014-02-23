a1af97a62da2b3d1daaa0ec9dea5d55c27d1e5f5
Sebastian Redl <sebastian.redl@getdesigned.at>
2014-02-24 00:32:56 +0100

    Implement reverse_with_position, for use by rotate.

--- include/accent/algorithms/reverse.hpp
+++ include/accent/algorithms/reverse.hpp
@@ -24,6 +24,33 @@ namespace accent { namespace algorithms {
       }
     }
 
+    template <typename RearrangeableBidirectionalRange>
+    typename RearrangeableBidirectionalRange::position
+    reverse_with_position(RearrangeableBidirectionalRange,
+                          typename RearrangeableBidirectionalRange::position,
+                          std::false_type);
+
+    template <typename RearrangeableBidirectionalRange>
+    typename RearrangeableBidirectionalRange::position
+    reverse_with_position(RearrangeableBidirectionalRange r,
+                          typename RearrangeableBidirectionalRange::position p,
+                          std::true_type) {
+      auto result = p;
+      while (!r.empty()) {
+        auto s = r;
+        s.drop_front();
+        if (!s.empty()) {
+          s.drop_back();
+          using std::swap;
+          swap(r.front(), r.back());
+          if (r.at_front() == p) result = r.at_back();
+          else if (r.at_back() == p) result = r.at_front();
+        }
+        r = s;
+      }
+      return result;
+    }
+
   }
 
   template <typename RearrangeableBidirectionalRange>
@@ -35,6 +62,23 @@ namespace accent { namespace algorithms {
                       RearrangeableBidirectionalRange>(),
                   "reverse requires a rearrangable range");
     detail::reverse(r,
+        support::bool_<support::BidirectionalRange<
+                           RearrangeableBidirectionalRange>() &&
+                       support::RearrangeableRange<
+                           RearrangeableBidirectionalRange>()>());
+  }
+
+  template <typename RearrangeableBidirectionalRange>
+  typename RearrangeableBidirectionalRange::position
+  reverse_with_position(RearrangeableBidirectionalRange r,
+                        typename RearrangeableBidirectionalRange::position p) {
+    static_assert(support::BidirectionalRange<
+                      RearrangeableBidirectionalRange>(),
+                  "reverse requires a bidirectional range");
+    static_assert(support::RearrangeableRange<
+                      RearrangeableBidirectionalRange>(),
+                  "reverse requires a rearrangable range");
+    return detail::reverse_with_position(r, p,
         support::bool_<support::BidirectionalRange<
                            RearrangeableBidirectionalRange>() &&
                        support::RearrangeableRange<
--- tests/algorithms/reverse.cpp
+++ tests/algorithms/reverse.cpp
@@ -17,3 +17,33 @@ INSTANTIATE_TEST_CASE_P(variousVectors, Reverse,
   ::testing::Values(vecp{{}, {}}, vecp{{1}, {1}}, vecp{{1, 2}, {2, 1}},
     vecp{{1, 2, 3}, {3, 2, 1}}, vecp{{1, 2, 3, 4}, {4, 3, 2, 1}},
     vecp{{1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}}));
+
+using spec = std::tuple<vec, int, vec>;
+
+class ReverseWithPosition : public ::testing::TestWithParam<spec> {};
+
+TEST_F(ReverseWithPosition, empty) {
+  vec v{};
+  auto pos = reverse_with_position(v.bidi(), v.bidi().at_front());
+  ASSERT_FALSE(pos.valid());
+}
+
+TEST_P(ReverseWithPosition, bidi) {
+  auto p = GetParam();
+  auto r = std::get<0>(p).bidi();
+  auto s = r;
+  for (int i = 0; i < std::get<1>(p); ++i) {
+    s.drop_front();
+  }
+  auto previous = s.front();
+  auto pos = reverse_with_position(r, s.at_front());
+  ASSERT_EQ(std::get<2>(p), std::get<0>(p));
+  ASSERT_TRUE(pos.valid());
+  ASSERT_EQ(previous, *pos);
+}
+
+INSTANTIATE_TEST_CASE_P(variousVectors, ReverseWithPosition,
+  ::testing::Values(spec{{1}, 0, {1}},
+    spec{{1, 2}, 0, {2, 1}}, spec{{1, 2}, 1, {2, 1}},
+    spec{{1, 2, 3}, 0, {3, 2, 1}}, spec{{1, 2, 3}, 1, {3, 2, 1}},
+    spec{{1, 2, 3}, 2, {3, 2, 1}}));
